4c45c5102a131eff267c66129f356ccb5c6cbffc
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-07-24 15:48:43 +0200

    scalable title  in header

--- sphinx_oe_theme/bootstrap-oe/static/oe-theme/js/oe-theme.js
+++ sphinx_oe_theme/bootstrap-oe/static/oe-theme/js/oe-theme.js
@@ -6,12 +6,17 @@ $( document ).ready(function () {
 });
 
 function resize_title(element){
+  console.log(element);
   var title = element.find('h1')[0];
+  console.log(title);
   var span = element.find('div.logo-block-title')[0];
+  console.log(span);
   var fontsize = $(title).css('font-size');
+  console.log(fontsize);
   $(title).css('font-size', parseFloat(fontsize) - 1);
   var padding = $(title).css('padding-top');
   $(title).css('padding-top', parseFloat(padding) + 0.5);
+  console.log($(title).width(), $(span).width());
   if ($(title).width() > $(span).width()){
     resize_title(element);
   }
